eef8f8ab0ec18b97a7638366f2b724ad41ed1eb3
Jemadean Dobrosielski <jemadean@gmail.com>
2018-08-21 06:11:52 -0700

    local authorization

---
+++ models/user.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  fullname: String,
+  password: { type: String, required: true}
+});
+
+// Transform output during `res.json(data)`, `console.log(data)` etc.
+userSchema.set('toObject', {
+  virtuals: true,
+  versionKey: false,
+  transform: (doc, ret) => {
+    delete ret._id;
+    delete ret.password;
+  }
+});
+
+userSchema.methods.validatePassword = function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+userSchema.statics.hashPassword = function (password) {
+  return bcrypt.hash(password, 10);
+};
+
+module.exports = mongoose.model('User', userSchema);
